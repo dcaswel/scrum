a95f9e9d696fd35712c67cda3e59b44ec4dd5ae7
Derek Caswell <dcaswell@goreact.com>
2022-08-26 17:53:52 -0600

    Created the form for editing a guideline. It still needs to be made functional.

--- resources/js/Pages/Guidelines/Edit.vue
+++ resources/js/Pages/Guidelines/Edit.vue
@@ -9,14 +9,8 @@
             <div class="bg-white overflow-hidden shadow sm:rounded-lg">
                 <div class="px-4 py-5 sm:p-6">
                     <ul class="divide-y divide-gray-200">
-                        <li v-for="score in scores">
-                            <div class="flex justify-between">
-                                <h2 class="text-gray-700 font-bold text-2xl mr-2 flex-1">{{score}}</h2>
-                                <button class="text-gray-400 hover:cursor-pointer">
-                                    <Pencil />
-                                </button>
-                            </div>
-                            <Guideline :guideline="getGuideline(score)" class="ml-10 mt-2"/>
+                        <li v-for="score in scores" class="py-2">
+                            <Guideline :guideline="getGuideline(score)" :key="'show' + score" class="mt-2"/>
                         </li>
                     </ul>
                 </div>
@@ -27,7 +21,6 @@
 
 <script setup>
 import AppLayout from "@/Layouts/AppLayout";
-import Pencil from "@/Components/icons/pencil";
 import Guideline from "@/Pages/Guidelines/Partials/Guideline"
 
 const props = defineProps({
@@ -35,7 +28,7 @@ const props = defineProps({
 });
 
 function getGuideline(score) {
-    return props.guidelines.find(guideline => guideline.score == score);
+    return props.guidelines.find(guideline => guideline.score == score) ?? {};
 }
 
 const scores = [0.5, 1, 2, 3, 5, 8, 13, 21];
--- resources/js/Pages/Guidelines/Partials/Guideline.vue
+++ resources/js/Pages/Guidelines/Partials/Guideline.vue
@@ -1,32 +1,126 @@
 <template>
     <div>
-        <div class="mb-4">
-            <div class="text-gray-900 font-bold">Description: </div>
-            <p class="pl-4">{{guideline.description}}</p>
+        <div class="flex justify-between">
+            <h2 class="text-gray-700 font-bold text-2xl mr-2 flex-1">{{ guideline.score }}</h2>
+            <button class="text-gray-400 hover:cursor-pointer" @click="editing = true">
+                <Pencil/>
+            </button>
         </div>
-        <div class="flex flex-col sm:flex-row justify-between pr-8">
-            <div>
-                <h3 class="text-gray-900 underline mb-2 mr-2">Bullet Items</h3>
-                <ul>
-                    <li v-if="guideline.bullets.length === 0" class="text-gray-400">No Bullet Items</li>
-                    <li v-for="bullet in guideline.bullets">- {{bullet.body}}</li>
-                </ul>
+        <div class="ml-10" v-if="!editing">
+            <div class="mb-4">
+                <div class="text-gray-900 font-bold">Description:</div>
+                <div class="bg-gray-50 overflow-hidden rounded-lg">
+                    <div class="px-4 py-5 sm:p-6">
+                        <p class="pl-4" v-if="guideline.description">{{ guideline.description }}</p>
+                        <p class="text-gray-400" v-else>No Description</p>
+                    </div>
+                </div>
             </div>
-            <div class="min-w-max">
-                <h3 class="text-gray-900 underline mb-2">Reference Tickets</h3>
-                <ul>
-                    <li v-if="guideline.tickets.length === 0" class="text-gray-400">No Tickets</li>
-                    <li v-for="ticket in guideline.tickets">- {{ticket.ticket_number}}</li>
-                </ul>
+            <div class="bg-gray-50 overflow-hidden rounded-lg">
+                <div class="px-4 py-5 sm:p-6">
+                    <div class="flex flex-col sm:flex-row justify-between pr-8">
+                        <div>
+                            <h3 class="text-gray-900 underline font-bold mb-2 mr-2">Bullet Items</h3>
+                            <ul>
+                                <li v-if="guideline.bullets?.length === 0" class="text-gray-400">No Bullet Items</li>
+                                <li v-for="bullet in guideline.bullets">- {{ bullet.body }}</li>
+                            </ul>
+                        </div>
+                        <div class="min-w-max">
+                            <h3 class="text-gray-900 underline font-bold mb-2">Reference Tickets</h3>
+                            <ul>
+                                <li v-if="guideline.tickets?.length === 0" class="text-gray-400">No Tickets</li>
+                                <li v-for="ticket in guideline.tickets">- {{ ticket.ticket_number }}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="ml-10" v-if="editing">
+            <div class="bg-white overflow-hidden shadow-lg rounded-lg border">
+                <div class="px-4 py-5 sm:p-6">
+                    <!-- Content goes here -->
+                    <div class="mb-2">
+                        <jet-label for="description" value="Description"/>
+                        <jet-input id="description" type="text" class="mt-1 block w-full" v-model="form.description"
+                                   autocomplete="description"/>
+                        <jet-input-error :message="form.errors.description" class="mt-2"/>
+                    </div>
+
+                    <divider message="Bullet Points"/>
+
+                    <div class="my-2">
+                        <ul>
+                            <li v-for="(bullet, index) in form.bullets">
+                                <div class="flex">
+                                    <jet-input :id="'bullet' + bullet.id" type="text" class="flex-1 mt-1 block" v-model="bullet.body"
+                                               autocomplete="body"/>
+                                    <button class="text-red-600 ml-2"><TrashIcon class="h-6 w-6"/></button>
+                                </div>
+                                <p class="text-sm text-red-600" v-if="form.errors[`bullets.${index}.body`]">{{form.errors[`bullets.${index}.body`]}}</p>
+                            </li>
+                            <li>
+                                <jet-secondary-button class="mt-2"><PlusCircleIcon class="w-6 h-6 mr-3" /> Add Bullet </jet-secondary-button>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <divider message="Reference Tickets"/>
+
+                    <div class="my-2">
+                        <ul>
+                            <li v-for="(ticket, index) in form.tickets">
+                                <div class="flex">
+                                    <jet-input :id="'ticket' + ticket.id" type="text" class="flex-1 mt-1 block"
+                                               v-model="ticket.ticket_number" autocomplete="body"/>
+                                    <button class="text-red-600 ml-2"><TrashIcon class="h-6 w-6"/></button>
+                                </div>
+                                <p class="text-sm text-red-600" v-if="form.errors[`tickets.${index}.ticket_number`]">{{form.errors[`tickets.${index}.ticket_number`]}}</p>
+                            </li>
+                            <li>
+                                <jet-secondary-button class="mt-2"><PlusCircleIcon class="w-6 h-6 mr-3" /> Add Ticket </jet-secondary-button>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+                    <div class="bg-gray-50 px-4 py-4 sm:px-6 text-right">
+                        <!-- Footer -->
+                        <jet-secondary-button @click="editing = false">Cancel</jet-secondary-button>
+                        <jet-button class="ml-3" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            Save
+                        </jet-button>
+                    </div>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+import TrashIcon from "@heroicons/vue/outline/TrashIcon";
+import PlusCircleIcon from "@heroicons/vue/outline/PlusCircleIcon";
+import Pencil from "@/Components/icons/pencil";
+import JetButton from '@/Jetstream/Button';
+import JetSecondaryButton from '@/Jetstream/SecondaryButton';
+import JetInput from '@/Jetstream/Input.vue'
+import JetInputError from '@/Jetstream/InputError.vue'
+import JetLabel from '@/Jetstream/Label.vue'
+import {Inertia} from "@inertiajs/inertia";
+import {ref} from "vue";
+import Divider from "@/Components/Divider";
+
 const props = defineProps({
     guideline: Object
 })
+
+let editing = ref(false);
+
+const form = Inertia.form({
+    description: props.guideline.description,
+    bullets: props.guideline.bullets,
+    tickets: props.guideline.tickets
+});
 </script>
 
 <style scoped>
